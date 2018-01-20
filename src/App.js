b1bebc8d528ca9a3015442b2d7a5198e4ae6aeb5
Drwn Me <admin@drwn.me>
2018-01-21 01:10:30 +1100

    using different map component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import {Map } from 'google-maps-react'
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+
+const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    onClick={props.onMapClick}
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+
+      {props.data.map(marker => (
+        <Marker
+          key={marker.lat}
+          position={{ lat: marker.lat, lng: marker.lng }}
+        />
+      ))}
+  </GoogleMap>
+));
+
 
 class App extends Component {
+  state = {
+    places: [{ lat: -34.397, lng: 150.644 }, { lat: -34.327, lng: 150.644 }]
+  }
   onMapClick = e => {
-    console.log('Current location!', e)
+    console.log('TELL me the LONG/LAT', e)
+    const { places } = this.state
+    // this.setState({places:[e.ea, ...places]})
   }
   render() {
     return (
       <div className="App">
       <br/><br/><br/><br/>
-        <Map onClick={this.onMapClick} google={window.google} />
+      <MapWithAMarker
+  onMapClick={e=>this.onMapClick(e)}
+  getPosition={e=>this.onMapClick(e)}
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJIsQq2P13hvXC12s7bdzvyb9btODQQNU&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+  data={this.state.places}
+/>
       </div>
     );
   }
